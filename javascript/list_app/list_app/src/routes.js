ce8a032f4ca612c65a2dc796d4b802a26b81b500
konradgnat <konradmgnat@gmail.com>
2018-05-15 22:39:26 -0500

    start structure

---
+++ javascript/list_app/list_app/src/routes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Route } from 'react-router';
+import App from 'modules/App/App';
+
+export default (
+      <Route exact path="/" component={App} >
+
+      </Route>
+)
